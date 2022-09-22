ced2c55b3aadbd4631c5a166c71c332dd0117556
Rodrigo C Boufleur <rcboufleur@gmail.com>
2022-09-22 14:40:49 -0300

    Create PETSc Vector and Matrix examples

---
+++ petsc_vec_mat_example.c
@@ -0,0 +1,46 @@
+//Based on https://www.youtube.com/watch?v=kP7DAOrqECo
+# include <petsc.h>
+
+int main{int argc, char **argv}
+{
+    // Create PETSc Vector Example
+    Vec b;
+    int j[4] = {0, 1, 2, 3};
+    double ab[4] = {7.0, 1.0, 2.0, 4.0};
+
+    PetscInitialize(&argc, &argv, NULL, "Create vector AND matrix");
+
+    VecCreate(PETSC_COMM_WORLD, &b);
+    VecSetSizes(b, PETSC_DECIDE, 4);
+    VecSetFromOptions(b);
+    VecSetValues(b, 4, j, ab, INSERT_VALUES);
+    VesAssemblyBegin(b); VecAssemblyEnd(b);
+
+    VecView(b, PETSC_VIEWER_STDOUT_WORLD);
+
+    VecDestroy(b);
+
+    // Create PETSc Matrix example
+    Mat A;
+    int i;
+    int jA[4] = {0, 1, 2, 3};
+    double aA[4][4] = { {7.0, 1.0, 1.0, 4.0},
+                        {5.0, 2.0, 9.0, 8.0},
+                        {8.0, 4.0, 4.0, 3.0},
+                        {9.0, 0.0, 6.0, 4.0} };
+
+    MatCreate(PETSC_COMM_WORLD, &A);
+    MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, 4, 4);
+    MatSetFromOptions(A);
+    MatSetUp(A);
+    for (1 = 0; i<4; i++){
+        MatSetValues(A, &i, 4, jA, aA[i], INSERT_VALUES);
+    }
+
+    MatAssemblyBegin(A); MatAssemblyEnd(A);
+
+    MatView(A, PETSC_VIEWER_STDOUT_WORLD);
+
+    MatDestroy(A);
+
+}
